908db4fa9915662289e8d80c84755723fadc541d
MatinGhafari <142246569+MatinGhafari@users.noreply.github.com>
2024-09-27 14:29:03 +0330

    Modifier commit

--- app/src/main/java/ir/matin/jetpackcompose/MainActivity.kt
+++ app/src/main/java/ir/matin/jetpackcompose/MainActivity.kt
@@ -1,17 +1,26 @@
 package ir.matin.jetpackcompose
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.currentCompositionLocalContext
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ir.matin.jetpackcompose.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,35 +29,27 @@ class MainActivity : ComponentActivity() {
 
         setContent {
 
-            CreateButton()
         }
     }
 }
-
+//use modifier for define more attributes for views
 @Composable
 fun Greeting(name: String) {
     Text(
-        text = "Hello $name!",
+        text = "$name!",
+        modifier = Modifier
+            .padding(10.dp)
+            .clickable {
+            }
+            .padding(10.dp)
+
+
     )
 }
-// create button
-//add
-@Composable
-fun CreateButton(){
-    Button(onClick = {
-    }) {
-        Greeting(name = "Matin")
 
 
-
-        
-    }
-
-
-}
-// show preview button
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-        CreateButton()
+    Greeting(name = "Matin")
 }
