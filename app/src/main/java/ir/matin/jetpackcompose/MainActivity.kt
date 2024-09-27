d6e646e33b7a5c80e830b6b23804234a04f4cd9c
MatinGhafari <142246569+MatinGhafari@users.noreply.github.com>
2024-09-27 22:01:41 +0330

    Surface commit

--- app/src/main/java/ir/matin/jetpackcompose/MainActivity.kt
+++ app/src/main/java/ir/matin/jetpackcompose/MainActivity.kt
@@ -7,17 +7,23 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.currentCompositionLocalContext
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
@@ -34,17 +40,28 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
+            MainScreen()
 
         }
     }
 }
 @Composable
-fun Greeting(name: String) {
-    // add style to text with textStyle
-    Text(text = name, modifier = Modifier.padding(20.dp), style = TextStyle(
-        color = Color.Blue , fontWeight = FontWeight.Bold , fontSize = 15.sp
+fun MainScreen() {
+    // using Surface : Surface is like ViewGroup in xml. we write our code into surface and manage it with functions.
+    Surface(color = Color.Cyan , modifier = Modifier.fillMaxSize() ) {
 
-    ))
+        Surface(color = Color.Blue , modifier = Modifier.fillMaxWidth().wrapContentSize(align = Alignment.TopCenter)) {
+
+            Text(text = "Hi mmm", style = MaterialTheme.typography.titleLarge,
+                modifier = Modifier.wrapContentSize(align = Alignment.TopCenter) )
+        }
+
+
+    }
+
+
+
+
 
 }
 
@@ -52,5 +69,5 @@ fun Greeting(name: String) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    Greeting(name = "Matin")
+  MainScreen()
 }
