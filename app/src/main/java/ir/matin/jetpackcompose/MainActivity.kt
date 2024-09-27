c14a3b64dddf3a5de90235a5c7391f972e44425b
MatinGhafari <142246569+MatinGhafari@users.noreply.github.com>
2024-09-27 11:42:43 +0330

    Initial commit

--- app/src/main/java/ir/matin/jetpackcompose/MainActivity.kt
+++ app/src/main/java/ir/matin/jetpackcompose/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,32 +17,37 @@ import ir.matin.jetpackcompose.ui.theme.JetpackComposeTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
-            JetpackComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+
+            CreateButton()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(name: String) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
     )
 }
+// create button
+@Composable
+fun CreateButton(){
+    Button(onClick = {
+    }) {
+        Greeting(name = "Matin")
 
+
+
+        
+    }
+
+
+}
+// show preview button
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    JetpackComposeTheme {
-        Greeting("Android")
-    }
+        CreateButton()
 }
