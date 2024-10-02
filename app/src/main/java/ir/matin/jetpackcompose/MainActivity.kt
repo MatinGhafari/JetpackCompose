1d8f7ab5e9cd57185a8d269dcb3bb2dd3460e996
MatinGhafari <142246569+MatinGhafari@users.noreply.github.com>
2024-10-02 17:55:27 +0330

    Add Theme commit

--- app/src/main/java/ir/matin/jetpackcompose/MainActivity.kt
+++ app/src/main/java/ir/matin/jetpackcompose/MainActivity.kt
@@ -4,49 +4,22 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModelProvider
-import ir.matin.jetpackcompose.ui.MainViewModel
-import ir.matin.jetpackcompose.ui.theme.JetpackComposeTheme
+import ir.matin.jetpackcompose.ui.theme.MyAppTheme
 
 
 class MainActivity : ComponentActivity() {
-    private lateinit var mainViewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
         setContent {
-            MainScreen()
+            // Use Custom Theme In our App
+            MyAppTheme {
+                MainScreen()
+
+
+            }
 
         }
     }
@@ -55,36 +28,19 @@ class MainActivity : ComponentActivity() {
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun MainScreen() {
-    Scaffold(topBar = { MyAppBar() }) {
-
-        Surface(modifier = Modifier.fillMaxSize()) {
-
-
-        }
-
-
-    }
 
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun MyAppBar() {
-    TopAppBar(
-        navigationIcon = { Icon(imageVector = Icons.Filled.Home, contentDescription = null) },
-        title = {
-            Text(
-                text = "Pizza Store", modifier = Modifier.fillMaxWidth().padding(16.dp)
-            )
-        }, colors = TopAppBarColors(Color.Cyan ,Color.Cyan ,Color.Black ,Color.Black ,Color.Gray )
-    )
 
-
-}
 
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    MainScreen()
+
+    MyAppTheme {
+        MainScreen()
+
+
+    }
 }
