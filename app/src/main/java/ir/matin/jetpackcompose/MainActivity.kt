9e9fe2f9b531e021e9c93a7fedafe63df5b1aae6
MatinGhafari <142246569+MatinGhafari@users.noreply.github.com>
2024-10-03 12:02:13 +0330

    Typography commit

--- app/src/main/java/ir/matin/jetpackcompose/MainActivity.kt
+++ app/src/main/java/ir/matin/jetpackcompose/MainActivity.kt
@@ -2,10 +2,18 @@ package ir.matin.jetpackcompose
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModelProvider
 import ir.matin.jetpackcompose.ui.theme.MyAppTheme
 
@@ -28,6 +36,19 @@ class MainActivity : ComponentActivity() {
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun MainScreen() {
+    Button(onClick = { /*TODO*/ } , modifier = Modifier
+        .padding(8.dp)
+        .width(100.dp)) {
+
+        Text(text = "Hello")
+        
+    }
+
+    Card {
+
+
+
+    }
 
 }
 
--- app/src/main/java/ir/matin/jetpackcompose/ui/theme/Theme.kt
+++ app/src/main/java/ir/matin/jetpackcompose/ui/theme/Theme.kt
@@ -8,18 +8,20 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = ColorMain,
+    secondary = ColorMain,
+    tertiary = ColorMain,
+
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = ColorMain,
+    secondary = ColorMain,
+    tertiary = ColorMain
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -52,6 +54,6 @@ fun MyAppTheme(
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
-        content = content
+        content = content,
     )
 }
