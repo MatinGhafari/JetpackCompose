a3e607dde25d9ec97d86abb1411b6469c152c97c
MatinGhafari <142246569+MatinGhafari@users.noreply.github.com>
2024-10-04 23:25:09 +0330

    LazyColumn commit

--- app/src/main/java/ir/matin/jetpackcompose/MainActivity.kt
+++ app/src/main/java/ir/matin/jetpackcompose/MainActivity.kt
@@ -1,20 +1,34 @@
 package ir.matin.jetpackcompose
 
 import android.annotation.SuppressLint
+import android.content.ClipData.Item
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModelProvider
+import ir.matin.jetpackcompose.ui.ItemList
+import ir.matin.jetpackcompose.ui.itemList2
 import ir.matin.jetpackcompose.ui.theme.MyAppTheme
 
 
@@ -24,7 +38,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             // Use Custom Theme In our App
             MyAppTheme {
-                MainScreen()
+                MainScreen(itemList2)
 
 
             }
@@ -33,26 +47,54 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun MainScreen() {
-    Button(onClick = { /*TODO*/ } , modifier = Modifier
-        .padding(8.dp)
-        .width(100.dp)) {
+fun ItemList(itemList: ItemList) {
+    Card(
+        modifier = Modifier
+            .padding(top = 8.dp)
+            .size(150.dp, 70.dp)
+    ) {
 
-        Text(text = "Hello")
-        
+
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            Image(
+                painterResource(id = itemList.image),
+                contentDescription = null,
+                modifier = Modifier.size(50.dp, 50.dp)
+            )
+            Text(text = itemList.title)
+
+
+        }
+
+
     }
 
-    Card {
-
-
-
-    }
 
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun MainScreen(data:List<ItemList>) {
+    Surface(modifier = Modifier.fillMaxSize(), color = Color.Gray) {
+        LazyColumn(verticalArrangement = Arrangement.Center , horizontalAlignment = Alignment.CenterHorizontally) {
+            items(data){
+             ItemList(itemList = it)
 
+
+
+            }
+        }
+
+
+    }
+
+
+}
 
 
 @Preview(showBackground = true)
@@ -60,8 +102,8 @@ fun MainScreen() {
 fun GreetingPreview() {
 
     MyAppTheme {
-        MainScreen()
 
+        MainScreen(itemList2)
 
     }
 }
---
+++ app/src/main/java/ir/matin/jetpackcompose/ui/ItemList.kt
@@ -0,0 +1,33 @@
+package ir.matin.jetpackcompose.ui
+
+import ir.matin.jetpackcompose.R
+
+data class ItemList(
+    val title: String,
+    val image:Int
+)
+
+val itemList2:List<ItemList> = listOf(
+    ItemList("image 1" , R.drawable.pic1),
+    ItemList("image 2" , R.drawable.pic2),
+    ItemList("image 3" , R.drawable.pic3),
+    ItemList("image 4" , R.drawable.pic1),
+    ItemList("image 5" , R.drawable.pic2),
+    ItemList("image 6" , R.drawable.pic3),
+    ItemList("image 7" , R.drawable.pic1),
+    ItemList("image 8" , R.drawable.pic2),
+    ItemList("image 9" , R.drawable.pic3),
+    ItemList("image 10" , R.drawable.pic1),
+    ItemList("image 11" , R.drawable.pic2),
+    ItemList("image 12" , R.drawable.pic3),
+    ItemList("image 13" , R.drawable.pic1),
+    ItemList("image 14" , R.drawable.pic2),
+    ItemList("image 15" , R.drawable.pic3),
+
+
+
+
+)
+
+
+
